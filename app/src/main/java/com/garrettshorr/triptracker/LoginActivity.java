4a07578694cededd2bb39ee8f95aed774c63a311
Garrett Shorr <g@garrettshorr.com>
2016-02-23 13:14:51 -0800

    Login functional, doesn't take you anywhere.

--- app/src/main/java/com/garrettshorr/triptracker/LoginActivity.java
+++ app/src/main/java/com/garrettshorr/triptracker/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mUserName;
     private EditText mPassword;
     private TextView mRegLink;
+    private Button mLoginButton;
 
     private static final int REGISTER_REQUEST_CODE = 1;
 
@@ -30,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         mUserName = (EditText) findViewById(R.id.enter_login);
         mPassword = (EditText) findViewById(R.id.enter_password);
         mRegLink = (TextView) findViewById(R.id.registration_link);
+        mLoginButton = (Button) findViewById(R.id.login_button);
 
         mRegLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +42,44 @@ public class LoginActivity extends AppCompatActivity {
                 startActivityForResult(registrationIntent, REGISTER_REQUEST_CODE);
             }
         });
+
+        mLoginButton.setOnClickListener(createNewLoginOnClickListener());
+
+    }
+
+    public View.OnClickListener createNewLoginOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence username = mUserName.getText();
+                CharSequence password = mPassword.getText();
+
+                if(isLoginValuesValid(username, password)) {
+                    LoadingCallback<BackendlessUser> loginCallback = createLoginCallback();
+                    loginCallback.showLoading();;
+                    loginUser( username.toString(), password.toString(), loginCallback);
+                }
+            }
+        };
+    }
+
+    private LoadingCallback<BackendlessUser> createLoginCallback() {
+        return new LoadingCallback<BackendlessUser>(this, getString(R.string.loading_login)) {
+            @Override
+            public void handleResponse(BackendlessUser loggedInUser) {
+                super.handleResponse(loggedInUser);
+                Toast.makeText(LoginActivity.this, String.format(getString(R.string.info_logged_in),
+                        loggedInUser.getObjectId()), Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    private boolean isLoginValuesValid(CharSequence username, CharSequence password) {
+        return Validator.isUsernameValid(this, username) && Validator.isPasswordValid(this, password);
+    }
+
+    private void loginUser(String username, String password, LoadingCallback<BackendlessUser> loginCallback) {
+        Backendless.UserService.login(username, password, loginCallback);
     }
 
     @Override
